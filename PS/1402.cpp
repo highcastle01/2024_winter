79461cda2640301e119093669ca7ab19f98a1c9d
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2024-01-01 20:14:48 +0900

    PS 업데이트

---
+++ PS/1402.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	int t, a, b; cin >> t;
+
+	while(t--)
+	{
+		int a, b; cin >> a >> b;
+
+		cout << "yes\n";
+	}
+
+	return 0;
+}
